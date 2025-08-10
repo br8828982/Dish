91f9dda6598e1bcdf544078c3e6a566ee60985a8
br8828982 <141433491+br8828982@users.noreply.github.com>
2025-08-10 13:02:51 +0530

    Delete app directory

--- app/src/main/Java/com/noor/dish/model/Channel.kt
+++
@@ -1,9 +0,0 @@
-package com.noor.dish.model
-
-data class Channel(
-    val id: String,
-    val title: String,
-    val logo_url: String,
-    val stream_url: String,
-    val license_url: String?,
-)
